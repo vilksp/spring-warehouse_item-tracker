93cde853de5903f1b19bd8764da624a1cc605fb7
vilksp <v.kasperavicius1@gmail.com>
2020-09-22 19:32:12 +0300

    initial commit

---
+++ src/main/java/ksp/vilius/Visma/task/VismaTaskApplication.java
@@ -0,0 +1,13 @@
+package ksp.vilius.Visma.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VismaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VismaTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ksp/vilius/Visma/task/VismaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ksp.vilius.Visma.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VismaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
